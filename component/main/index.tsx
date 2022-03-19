cc248587a2b4a42bb2e65ff061db873d3f1def16
张圣群 <1284773109@qq.com>
2022-03-19 09:53:28 +0800

    '文件解构设计'

---
+++ component/main/index.tsx
@@ -0,0 +1,8 @@
+function Main() {
+    return (
+        <div id="main">
+
+        </div>
+    )
+}
+export default Main
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Main from "../component/main";
+
+function App() {
+  return (
+    <div className="App">
+      {/* // 数学表达式黑板 */}
+      <div ></div>
+      {/* 聊天界面 */}
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
